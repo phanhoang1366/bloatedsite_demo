1efe13e08d1096b40172b6bd3ffcf7e0c65ecd8a
Phan Hoang <phanhoang1366@gmail.com>
2023-06-26 10:53:59 +0700

    fixing things

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ import secrets
 app = Flask(__name__, template_folder=".")
 app.secret_key = secrets.token_bytes()
 
-css_file = open("css/output.css", "r")
-css_contents = css_file.read()
-css_file.close()
-
 @app.after_request
 def after_request_callback(response: Response):
   print(response.__dict__)
@@ -26,14 +22,17 @@ def home():
     return "OK", 200
     return render_template("template.html", status=200, message="OK")
 
-@app.route("/css/output.css")
-def css_content():
-    return css_contents, 200, {'Content-Type': 'text/css'}
+@app.route('/css/<path:filename>')
+def css_content(filename):
+    return send_from_directory('css', filename)
 
 @app.route("/index.js")
 def get_playlist():
     return send_from_directory('.', 'index.js')
 
+@app.route("/undefined")
+def empty_response():
+    return ('', 204)
 @app.route('/audio/<path:filename>')
 def serve_music(filename):
     return send_from_directory('audio', filename)
